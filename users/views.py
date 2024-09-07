ed8a870663e194b454a38a0809766838149434c4
Renntor <denis30510@gmail.com>
2024-09-07 19:02:21 +0300

    Create User model, create migrations.

---
+++ users/views.py
@@ -0,0 +1,14 @@
+from users.models import User
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from users.serializers import UserSerializers
+
+
+class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAdminUser]
+    serializer_class = UserSerializers
+
+class UserListCreateAPIView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
